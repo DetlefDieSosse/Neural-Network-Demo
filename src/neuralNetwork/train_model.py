8d59196fe066fbd4d75a29b86f9e56ed38b66e4a
I750476 <e.lentge@gmx.de>
2025-02-18 22:21:36 +0100

    shit

--- src/neuralNetwork/train_model.py
+++ src/neuralNetwork/train_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models, Input # type: ignore
 from tensorflow.keras.callbacks import EarlyStopping # type: ignore
+from tensorflow.keras.preprocessing import image # type: ignore
 
 # Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -13,22 +14,33 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = np.expand_dims(x_train, axis=-1)
 x_test = np.expand_dims(x_test, axis=-1)
 
+# Data augmentation
+datagen = image.ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+datagen.fit(x_train)
+
 def build_and_train_model():
     # Define the model architecture
     model = models.Sequential([
         Input(shape=(28, 28, 1)),
 
-        layers.Conv2D(32, (3, 3), activation='relu'),
+        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
         layers.MaxPooling2D((2, 2)),
         
-        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
         layers.MaxPooling2D((2, 2)),
         
-        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+        
         layers.Flatten(),
         
-        layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0001)),
-        layers.Dropout(0.3),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
         layers.Dense(10, activation='softmax')
     ])
 
@@ -38,10 +50,13 @@ def build_and_train_model():
                   metrics=['accuracy'])
 
     # Early stopping to prevent overfitting
-    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
     
-     # Train the model
-    model.fit(x_train, y_train, batch_size=32, epochs=50, validation_data=(x_test, y_test), callbacks=[early_stopping])
+    # Train the model
+    model.fit(datagen.flow(x_train, y_train, batch_size=128), 
+              epochs=100, 
+              validation_data=(x_test, y_test), 
+              callbacks=[early_stopping])
 
     # Evaluate the model
     test_acc = model.evaluate(x_test, y_test, verbose=2)
